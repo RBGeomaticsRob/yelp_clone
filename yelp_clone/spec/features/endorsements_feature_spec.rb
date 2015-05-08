ff1f962672868869750ba683ce6d77c9f1b8777f
icicleta <icicleta@gmail.com>
2015-05-08 18:11:27 +0100

    create test for more than one endorsement

--- yelp_clone/spec/features/endorsements_feature_spec.rb
+++ yelp_clone/spec/features/endorsements_feature_spec.rb
@@ -6,9 +6,16 @@ feature 'endorsing reviews' do
     kfc.reviews.create(rating: 3, thoughts: 'It was a abomination')
   end
 
-  scenario 'a user can endorse a review which updates the review endorsment count' do
+  scenario 'a user can endorse a review which updates the review endorsement count' do
     visit '/restaurants'
     click_link('Endorse KFC')
-    expect(page).to have_content('1 endorsment')
+    expect(page).to have_content('1 endorsement')
+  end
+
+  scenario 'a user can endorse a review which updates the review endorsement count' do
+    visit '/restaurants'
+    click_link('Endorse KFC')
+    click_link('Endorse KFC')
+    expect(page).to have_content('2 endorsements')
   end
 end
